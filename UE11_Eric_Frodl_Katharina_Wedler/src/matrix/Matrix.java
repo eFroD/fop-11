1977ad84616f6d1f34f14fab8eff57cdaa4f22f0
Kathy <katharina.wedler@stud.tu-darmstadt>
2018-01-24 10:47:50 +0100

    wuhu

--- UE11_Eric_Frodl_Katharina_Wedler/src/matrix/Matrix.java
+++ UE11_Eric_Frodl_Katharina_Wedler/src/matrix/Matrix.java
@@ -1,4 +1,18 @@
 package matrix;
 
 public class Matrix <T extends Comparable<T>> {
+    private Arithmetic<T> arithmetic;
+    private LikedList<LinkedList<T>> data;
+    private int rows, columns;
+
+    public Matrix(int rows, int columns, Arithmetic<T> arithmetic){
+        rows = this.rows;
+        columns = this.columns;
+    }
+
+    public int getRows() return rows;
+    public int getColumns() return columns;
+    public T getCell(int row, int column){}
+    public void setCell(int row, int column, T value){}
+
 }
